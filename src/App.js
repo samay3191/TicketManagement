79155468aaaf6c1c6c34523bbd3783d62c52023a
Samay Bhavsar <samay3191@gmail.com>
2018-10-25 23:27:11 +0530

    Done some cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import CountryList from './js/components/CountryListPage';
+import { Link } from 'react-router';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from './js/actions/filterActions';
@@ -17,87 +17,21 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      searchText: "",
-      showCountries: false,
-      filteredCounties: this.props.countries,
-      selectedIndex: 0
-    };
   }
-
-  componentWillReceiveProps (newProps) {
-    this.setState({ filteredCounties: newProps.countries });
-  }
-
-  // fires on text change
-  searchTextChange = (event) => {
-    if (event.target.validity.valid) {
-      const searchTerm = event.target.value.trim();
-      const showCountries = searchTerm.length > 0;
-      this.setState({
-        searchText: searchTerm,
-        showCountries,
-        selectedIndex: 0
-      });
-      setTimeout(() => this.props.filterCountries(searchTerm), 500);
-    }
-  }
-
-  setCountry = country => {
-    this.setState({
-      searchText: country,
-      showCountries: false,
-      selectedIndex: 0
-    });
-  }
-
-  // to select country from the list
-  onSearchTextKeyDown = key => {
-    const { selectedIndex, filteredCounties } = this.state;
-    if (key.keyCode === 13) { //enter
-      if (this.state.searchText && this.state.searchText.length > 0) {
-        this.setCountry(filteredCounties[selectedIndex].name);
-      }
-    }
-    else if (key.keyCode === 38) { //up
-      if (selectedIndex === 0) {
-        return;
-      }
-      this.setState({ selectedIndex: selectedIndex - 1 });
-    }
-    else if (key.keyCode === 40) { //down
-      if (selectedIndex + 1 === filteredCounties.length) {
-        return;
-      }
-      this.setState({ selectedIndex: selectedIndex + 1 });
-    }
-  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <div>
-            <h1>Auto Complete Demo</h1>
+            <h1>React Demo</h1>
           </div>
           <div>
-            <input
-              id="searchBox"
-              type="text"
-              onChange={this.searchTextChange}
-              onKeyDown={this.onSearchTextKeyDown}
-              value={this.state.searchText}
-              className="searchBox"
-              pattern="[A-Za-z]*"
-            />
+            <Link to="/AddTicket">Add Ticket</Link>
+            <Link to="/TicketList">Ticket List</Link>
+            <Link to="/AssignTicket">Assign Ticket</Link>
           </div>
-          { this.state.showCountries &&
-            <CountryList
-              countries={this.state.filteredCounties}
-              selectedIndex={this.state.selectedIndex}
-              setCountry={this.setCountry}
-            />
-          }
+          {this.props.children}
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
 import store from "./js/store/index";
 import { Router, Route, hashHistory } from 'react-router'
+import AddTicket from './js/components/AddTicket';
+import AssignTicket from './js/components/AssignTicket';
+import TicketList from './js/components/TicketList';
 
 ReactDOM.render(<Provider store={store}>
     <Router history={hashHistory}>
-      <Route path="/" component={App}/>
+      <Route path="/" component={App}>
+        <Route path="AddTicket" component={AddTicket}/>
+        <Route path="TicketList" component={TicketList}/>
+        <Route path="AssignTicket" component={AssignTicket}/>
+      </Route>
     </Router>
   </Provider>, document.getElementById('root'));
 
